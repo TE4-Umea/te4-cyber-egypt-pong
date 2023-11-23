3ca4373f0aa524e920719e7c5015917d47ce8c2a
BeepingSeath <Beeping.Seath.DKS@gmail.com>
2023-11-23 10:48:20 +0100

    Bot fails sometimes but it just stops so it doesn't look very natural

--- Scenes/Enemy.gd
+++ Scenes/Enemy.gd
@@ -3,11 +3,20 @@ extends CharacterBody2D
 
 @export var speed : float = 1000.0
 var paused = false
+var rng = RandomNumberGenerator.new()
+
+var bot_fail = rng.randi_range(1, 5)
+var bot_success = 0
+
+func bot_reset():
+	bot_fail = rng.randi_range(1, 5)
+	bot_success = 0
+	print(bot_fail)
 
 func _physics_process(delta):
 	var direction
 	if !paused:
-		direction = get_axis('Player_UP', 'Player_DOWN')
+		direction = get_axis()
 		if direction:
 			velocity.y = direction * speed
 		else:
@@ -15,15 +24,25 @@ func _physics_process(delta):
 		
 		move_and_slide()
 
-func get_axis(up, down):
+func get_axis():
 	var ball_position = get_parent().get_node("Ball").position
-	if position.y < ball_position.y: return 1
-	elif position.y > ball_position.y: return -1
+	if bot_success < bot_fail:
+		if position.y < ball_position.y: return 1
+		elif position.y > ball_position.y: return -1
 
 
 func _on_area_2d_body_entered(body):
 	body.direction.x *= -1
+	bot_success += 1
 
 
 func _on_world_pause_signal():
 	paused = true
+
+
+func _on_fail(body):
+	bot_reset()
+
+
+func _on_success(body):
+	bot_reset()
