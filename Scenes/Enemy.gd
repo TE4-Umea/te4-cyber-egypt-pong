9b3bae97cad8b134e9f29733e2bbce100803bd93
BeepingSeath <Beeping.Seath.DKS@gmail.com>
2023-11-24 10:25:06 +0100

    Enemy becomes transparent instead and ball goes through

--- Scenes/Enemy.gd
+++ Scenes/Enemy.gd
@@ -7,6 +7,12 @@ var rng = RandomNumberGenerator.new()
 
 var bot_fail = rng.randi_range(1, 5)
 var bot_success = 0
+var size = null
+var vulnerable_state = null
+
+func _ready():
+	size = $"."
+	vulnerable_state = $Sprite2D
 
 func bot_reset():
 	bot_fail = rng.randi_range(1, 5)
@@ -26,14 +32,21 @@ func _physics_process(delta):
 
 func get_axis():
 	var ball_position = get_parent().get_node("Ball").position
+	
 	if bot_success < bot_fail:
+		modulate.a = 1
+		if position.y < ball_position.y: return 1
+		elif position.y > ball_position.y: return -1
+	else:
+		modulate.a = 0.5
 		if position.y < ball_position.y: return 1
 		elif position.y > ball_position.y: return -1
 
 
 func _on_area_2d_body_entered(body):
-	body.direction.x *= -1
-	bot_success += 1
+	if bot_success < bot_fail:
+		body.direction.x *= -1
+		bot_success += 1
 
 
 func _on_world_pause_signal():
