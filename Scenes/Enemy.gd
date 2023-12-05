e997b9f02fee0421a2c4bc1204bc0700746ed2d9
BeepingSeath <Beeping.Seath.DKS@gmail.com>
2023-12-05 13:59:21 +0100

    Paddles bouncing ball correctly, scoring health based, renaming of horrible objects

--- Scenes/Enemy.gd
+++ Scenes/Enemy.gd
@@ -1,25 +1,42 @@
 extends CharacterBody2D
 
 
-@export var speed : float = 1000.0
+@export var speed : float = 250.0
 var paused = false
 var rng = RandomNumberGenerator.new()
 var screensize
 
 var bot_fail = rng.randi_range(1, 5)
 var bot_success = 0
-var size = null
-var vulnerable_state = null
+var max_bounce_angle = 5*PI/24
+var recently_hit = false
 
 func _ready():
 	screensize = get_viewport().get_visible_rect().size
-	size = $"."
-	vulnerable_state = $Sprite2D
 
 func bot_reset():
 	bot_fail = rng.randi_range(1, 5)
 	bot_success = 0
 	print(bot_fail)
+
+func bounce(body):
+	var collision : CollisionShape2D = $Area2D/CollisionShape2D
+	var collision_height = collision.shape.get_rect().size.y
+	
+	var dist = (position.y - body.position.y)
+	var normalizedDist = dist/(collision_height/2)
+	
+	var bounceAngle = -1 * normalizedDist * max_bounce_angle
+	var dir
+	if body.direction.x < 0:
+		dir = 1
+	else:
+		dir = -1
+	if !recently_hit:
+		body.direction = Vector2(cos(bounceAngle) * dir, sin(bounceAngle))
+		recently_hit = true
+		$RecentHitTimer.start()
+
 
 func _physics_process(delta):
 	var direction
@@ -35,23 +52,27 @@ func _physics_process(delta):
 func get_axis():
 	var ball_position = get_parent().get_node("Ball").position
 	
-	if bot_success < bot_fail && ball_position.x < position.x:
+	if bot_success < bot_fail:
 		modulate.a = 1
-		if position.y < ball_position.y: return 1
-		elif position.y > ball_position.y: return -1
 	else:
 		modulate.a = 0.25
+
+	if bot_success < bot_fail && ball_position.x < position.x:
+		if position.y < ball_position.y: return 1
+		elif position.y > ball_position.y: return -1
 
 
 func _on_area_2d_body_entered(body):
 	if bot_success < bot_fail:
-		body.direction.x *= -1
 		bot_success += 1
+		bounce(body)
 
 
 func _on_world_pause_signal():
 	paused = true
 
+func _on_recent_hit_timer_timeout():
+	recently_hit = false
 
 func _on_fail(body):
 	bot_reset()
